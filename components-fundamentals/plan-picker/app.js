eef88ccba33ada58f5a85634fb5473d5d6b027d7
Kanghyun Jo <kjo0@outlook.com>
2021-10-10 21:05:46 -0400

    Reusable components with props

---
+++ components-fundamentals/plan-picker/app.js
@@ -0,0 +1,14 @@
+const app = Vue.createApp({
+	data() {
+		return {
+			plans: ['The Single', 'The Curious', 'The Addict'],
+		};
+	},
+})
+	.component('plan', {
+		template: '#plan-template',
+		props: {
+			name: { type: String, required: true },
+		},
+	})
+	.mount('#app');

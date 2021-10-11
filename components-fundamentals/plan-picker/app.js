fa98724e45c739551e34889bf45863f106aa67e3
Kanghyun Jo <kjo0@outlook.com>
2021-10-11 19:31:48 -0400

    Nested components

--- components-fundamentals/plan-picker/app.js
+++ components-fundamentals/plan-picker/app.js
@@ -1,10 +1,12 @@
-const app = Vue.createApp({
-	data() {
-		return {
-			plans: ['The Single', 'The Curious', 'The Addict'],
-		};
-	},
-})
+const app = Vue.createApp({})
+	.component('plan-picker', {
+		template: '#plan-picker-template',
+		data() {
+			return {
+				plans: ['The Single', 'The Curious', 'The Addict'],
+			};
+		},
+	})
 	.component('plan', {
 		template: '#plan-template',
 		props: {
